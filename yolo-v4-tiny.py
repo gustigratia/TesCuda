6a2a67360254355597f5e4849310f9238ad52e54
cthleen <154051577+cthleen@users.noreply.github.com>
2024-05-20 21:41:32 +0700

    Update yolo-v4-tiny.py

--- yolo-v4-tiny.py
+++ yolo-v4-tiny.py
@@ -6,7 +6,7 @@ NMS_THRESHOLD = 0.4
 COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
 # Load YOLO network
-net = cv2.dnn.readNet("custom-yolov4-tiny-detector_final.weights", "custom-yolov4-tiny-detector.cfg")
+net = cv2.dnn.readNet("/dataset/custom-yolov4-tiny-detector_final.weights", "/dataset/custom-yolov4-tiny-detector.cfg")
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
 
